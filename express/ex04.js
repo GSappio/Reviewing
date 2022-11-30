41e33521a6926d62c972e374d8eeb8448a4f0ede
Gabriel <gabriel.sappio02@gmail.com>
2022-11-30 12:05:08 -0300

    Reviewing express router

--- express/ex04.js
+++ express/ex04.js
@@ -5,7 +5,6 @@ server.route('/clientes')
     .get((req, res) => res.send('Lista de Clientes'))
     .post((req, res) => res.send('Novo Cliente'))
     .put((req, res) => res.send('Alterar Cliente'))
-    .delete((req, res) => res.send('Remove Cliente'))
+    .delete((req, res) => res.send('Remove Cliente')) 
      
-
 server.listen(3000, () => console.log('Executando...'))
