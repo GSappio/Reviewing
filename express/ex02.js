cf823294f9b313ae679af1f948fc4e40a11989ac
Gabriel <gabriel.sappio02@gmail.com>
2022-12-02 11:49:03 -0300

    Creating basic webpack structure

--- express/ex02.js
+++ express/ex02.js
@@ -10,6 +10,6 @@ server.get('/', function(req, res, next) {
 server.get('/', function(req, res) {
     console.log('Resposta...')
     res.send('<h1>Olá Express</h1>')
-})
+})  
 
 server.listen(3000, () => console.log('Executando...'))
