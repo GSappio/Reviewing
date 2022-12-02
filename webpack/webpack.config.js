4b0f6a104094e4f5d914fcbbd422e6c459076dfb
Gabriel <gabriel.sappio02@gmail.com>
2022-12-02 12:14:25 -0300

    Loading .css files

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,5 +6,15 @@ module.exports = {
   output: {
       filename: 'principal.js',
       path: __dirname + '/public'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader', // Adiciconar css a dom injetando a tag <style>
+        'css-loader' // interpretar @import, url()...
+      ]
+    }]
   }
+
 };
