152d79530171844050a5dc84eec8484cfcb0f96a
Gabriel <gabriel.sappio02@gmail.com>
2022-12-02 12:31:40 -0300

    Externalizing .css files

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
-const webpack = require('webpack')
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,12 +7,19 @@ module.exports = {
   output: {
       filename: 'principal.js',
       path: __dirname + '/public'
-  },
+  }, 
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "estilo.css"
+    })
+  ],
+
   module: {
     rules: [{
       test: /\.css$/,
       use: [
-        'style-loader', // Adiciconar css a dom injetando a tag <style>
+        MiniCssExtractPlugin.loader,
+        // 'style-loader', // Adiciconar css a dom injetando a tag <style>
         'css-loader' // interpretar @import, url()...
       ]
     }]
