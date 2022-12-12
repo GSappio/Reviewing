c9bddd55704be670864d2216593f50cb65edca9d
GabrielSappio <gabriel.sappio02@gmail.com>
2022-12-12 11:31:02 -0300

    Adding the react preset

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -1,24 +1,7 @@
-const produto = {
-    nome: 'Caneta Bic Preta',
-    preco: 1.90,
-    desconto: 0.05
+import React from "react";
+
+export  default props => {
+    <h1>Olá</h1>
 }
 
-function clone(objeto) {
-    return { ...objeto }
-}
-
-const novoProdutoDesconto = clone(produto)
-novoProdutoDesconto.nome = 'Caneta Bic Azul'
-novoProdutoDesconto.desconto = 0.1
-
-const novoValor = clone(produto)
-novoValor.preco = 2.50
-
-const novoDesconto = clone(produto)
-novoDesconto.desconto = 0.1
-
-
-console.log(produto, novoProdutoDesconto)
-console.log(produto, novoValor)
-console.log(produto, novoDesconto)
+console.log('Funcionou!')
---
+++ exercicios_webpack/ex3/index.js
@@ -0,0 +1,24 @@
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+}
+
+function clone(objeto) {
+    return { ...objeto }
+}
+
+const novoProdutoDesconto = clone(produto)
+novoProdutoDesconto.nome = 'Caneta Bic Azul'
+novoProdutoDesconto.desconto = 0.1
+
+const novoValor = clone(produto)
+novoValor.preco = 2.50
+
+const novoDesconto = clone(produto)
+novoDesconto.desconto = 0.1
+
+
+console.log(produto, novoProdutoDesconto)
+console.log(produto, novoValor)
+console.log(produto, novoDesconto)
